0ab51619f2a9acfeaaf294cbd575247602384413
NEWYORKHEART911 <lavillelumiere8@gmail.com>
2023-01-26 03:15:56 +0000

    changes2

--- src/main/java/IfStatement.java
+++ src/main/java/IfStatement.java
@@ -20,10 +20,6 @@ public class IfStatement {
      * @return x if bool is true. if bool is false, return y.
      */
     public int ifExample(boolean bool, int x, int y){
-        if(bool) {
-            return x;
-        } else {
-            return y;
-        }
+        return (bool) ? x : y;
     }
 }
